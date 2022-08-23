8151c0fac9deb216d36fa63dd4beba6a5bc13d13
malikris <malikris@fit.cvut.cz>
2022-08-23 12:09:51 +0200

    Added qorus login functionality with tests

---
+++ lib/QorusToolkit.ts
@@ -0,0 +1,67 @@
+"use strict";
+
+import logger from "./logger";
+import httpsAxios from "../utils/httpsAxios";
+
+interface ILoginParams {
+  url?: string;
+  user: string;
+  pass: string;
+}
+
+class QorusToolkit {
+  usrToken?: string;
+  endpoint: string =
+    "https://hq.qoretechnologies.com:31011/api/latest/public/login";
+
+  async login(params: ILoginParams) {
+    const {
+      user,
+      pass,
+    } = params;
+
+    try {
+      const resp = await httpsAxios({
+        method: "post",
+        url: this.endpoint,
+        data: { user: user, pass: pass },
+      });
+      logger.info(`User signed in with token : ${JSON.stringify(resp.data)}`);
+      this.usrToken = resp.data;
+      return resp.data;
+    } catch (error: any) {
+      logger.error(
+        `Couldn't sign in user, ErrorCode: ${error.code}, ErrorMessage: ${error.message}`
+      );
+      throw new Error(
+        `Couldn't sign in user, ErrorCode: ${error.code}, ErrorMessage: ${error.message}`
+      );
+    }
+  }
+
+  logout() {
+    this.usrToken = undefined;
+  }
+
+  config(endpoint: string) {
+    if (this.usrToken) {
+      this.logout();
+    }
+    this.endpoint = endpoint;
+    logger.info("Configurations changed please login again");
+  }
+
+  getConfig() {
+    return this.endpoint;
+  }
+
+  getUserToken() {
+    if (this.usrToken) return this.usrToken;
+    else {
+      logger.error("User is not signed in...");
+      return null;
+    }
+  }
+}
+
+export default QorusToolkit;
---
+++ test/QorusToolkit.test.ts
@@ -0,0 +1,85 @@
+"use strict";
+
+import QorusToolkit from "../lib/QorusToolkit";
+import MockAdapter from "axios-mock-adapter";
+import httpsAxios from "../utils/httpsAxios";
+import logger from "../lib/logger";
+
+describe("QorusToolkit Utility Class Tests", () => {
+  let mock: MockAdapter;
+  const qorus = new QorusToolkit();
+
+  beforeAll(() => {
+    mock = new MockAdapter(httpsAxios);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  describe("When authentication is successful", () => {
+    it("Should return user token after authentication", async () => {
+      const usrToken = "336d9306-3b70-41df-9427-c051d25886b2";
+      mock
+        .onPost(
+          "https://hq.qoretechnologies.com:31011/api/latest/public/login",
+          {
+            user: "rmalik",
+            pass: "yzu2d8smRoCetW8",
+          }
+        )
+        .reply(200, usrToken);
+
+      let result;
+      try {
+        result = await qorus.login({
+          url: "https://hq.qoretechnologies.com:31011/api/latest/public/login",
+          user: "rmalik",
+          pass: "yzu2d8smRoCetW8",
+        });
+      } catch (error: any) {
+        logger.error(`${error.code} ${error.message}`);
+      }
+
+      expect(mock.history.post[0].data).not.toBeNull();
+      expect(result).toEqual(usrToken);
+    });
+
+    it("should return the current auth config", () => {
+      const config = qorus.getConfig();
+
+      expect(config).toEqual(
+        "https://hq.qoretechnologies.com:31011/api/latest/public/login"
+      );
+    });
+
+    it("Should return current user token if the user is authenticated", () => {
+      expect(qorus.getUserToken()).not.toBeNull();
+    });
+
+    it("Should change the config", () => {
+      qorus.config("https://www.google.com");
+
+      expect(qorus.getConfig()).toEqual("https://www.google.com");
+    });
+
+    it("Should logout the user", () => {
+      qorus.logout();
+
+      expect(qorus.getUserToken()).toEqual(null);
+    });
+  });
+
+  describe("When authentication is not successful", () => {
+    it("Should return error if the user can't sign in", async () => {
+      let err = { message: "", code: "" };
+      try {
+        await qorus.login({ user: "bob", pass: "lsjdfkl" });
+      } catch (error: any) {
+        err.message = error.message;
+      }
+
+      expect(err.message).toContain("Couldn't sign in user,");
+    });
+  });
+});
