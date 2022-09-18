2803847708fa3f81cde0e78717408c1a4281d882
Rishabh Malik <malikris@fit.cvut.cz>
2022-09-18 15:59:49 +0000

    Updated updateDocs script

--- updateDocs.sh
+++ updateDocs.sh
@@ -5,6 +5,6 @@ echo $VERSION
 for f in docs/**/*
 do
  if [[ $f == *.html ]]; then
-  sed -i -e "s|@qoretechnologies/qorus-toolkit|@qoretechnologies/qorus-toolkit@pull_request_$VERSION|g" $f
+  sed -i -e "s|@qoretechnologies/qorus-toolkit|@qoretechnologies/qorus-toolkit@$VERSION|g" $f
  fi
 done
