03c684be66908223835c3e39c025e04e64b3404f
Rishabh Malik <malikris@fit.cvut.cz>
2022-10-06 09:19:03 +0200

    Updated export to include missing types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Authenticator, Endpoint, InitEndpoint, LoginParams, QorusAuthenticator } from './QorusAuthenticator';
-
+export { Version } from './utils/apiPaths';
 export { QorusReq } from './utils/QorusRequest';
