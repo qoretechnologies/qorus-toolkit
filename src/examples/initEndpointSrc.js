489c0ba7c43e2954aa6a2d8b2f3a407730257ab3
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-10-11 13:45:03 +0000

    Updated initEndpoint example

--- src/examples/initEndpointSrc.js
+++ src/examples/initEndpointSrc.js
@@ -1,7 +1,10 @@
 // Initializes a new endpoint and returns it
+// Optional user and pass parameters can be provided to initialize the endpoint and then authenticate the user
 await Qorus.orusAuthenticator.initEndpoint({
   id: 'rippy',
   url: 'https://sandbox.qoretechnologies.com',
   version: 'latest',
+  user: 'sandbox',
+  pass: 'sandbox',
 });
 // => {"url":"https://sandbox.qoretechnologies.com","id":"rippy","version":"latest"}
