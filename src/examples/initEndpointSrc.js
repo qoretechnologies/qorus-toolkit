e56d3d11d87e29979d9a9d823ee4a83d9f708dc7
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-10-11 13:49:39 +0000

    Updated Example

--- src/examples/initEndpointSrc.js
+++ src/examples/initEndpointSrc.js
@@ -1,6 +1,6 @@
 // Initializes a new endpoint and returns it
 // Optional user and pass parameters can be provided to initialize the endpoint and then authenticate the user
-await Qorus.orusAuthenticator.initEndpoint({
+await Qorus.QorusAuthenticator.initEndpoint({
   id: 'rippy',
   url: 'https://sandbox.qoretechnologies.com',
   version: 'latest',
