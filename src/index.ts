22a203b2d0d098b4525597f27e73e18c1f1033d4
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-10-27 17:11:03 +0000

    Updated docs and release version

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import QorusRequest from './QorusRequest';
 import QorusDataProvider from './QorusDataProvider';
 
 export { Version } from './utils/apiPaths';
-export * as QorusAuth from './QorusAuthenticator';
-export * as QorusReq from './QorusRequest';
+export * as Authenticator from './QorusAuthenticator';
+export * as HttpRequest from './QorusRequest';
 export * as DataProvider from './QorusDataProvider';
 
 export { QorusAuthenticator };
