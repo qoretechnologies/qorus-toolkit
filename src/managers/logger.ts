80e7b08462db0436de8c5b894c7ccd7df7011553
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-11-23 08:07:21 +0100

    Added browser support for roarr

--- src/managers/logger.ts
+++ src/managers/logger.ts
@@ -1,3 +1,10 @@
 import { Roarr as logger } from 'roarr';
+import { detect } from 'detect-browser';
+const browser = detect();
 
+if (browser) {
+  logger.prototype.write = (message) => {
+    console.log(JSON.parse(message));
+  };
+}
 export default logger;
