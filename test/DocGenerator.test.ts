efaea5950262354e143e9a60c043ad95e3460d14
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-12-14 22:13:09 +0100

    Fixed tests

--- test/DocGenerator.test.ts
+++ test/DocGenerator.test.ts
@@ -3,17 +3,17 @@ describe('Generate Docs class tests', () => {
   jest.setTimeout(50000);
   it('Check if documentation object exist', async () => {
     const classObj = DocGenerator.getClass('QorusAuthenticator');
-    expect(DocGenerator.getMethodDocs('addEndpoint', classObj)).not.toEqual(undefined);
+    expect(DocGenerator.createMethodDocs('addEndpoint', classObj)).not.toEqual(undefined);
   });
   it('Generate docs for Classes', async () => {
-    const classDocs = DocGenerator.getClassDocs('QorusOptions');
+    const classDocs = DocGenerator.createClassDocs('QorusOptions');
     expect(classDocs?.hasOwnProperty('name')).toEqual(true);
     expect(classDocs?.hasOwnProperty('comment')).toEqual(true);
     expect(classDocs?.hasOwnProperty('properties')).toEqual(true);
   });
   it('Generate docs for Methods', () => {
     const classObj = DocGenerator.getClass('QorusAuthenticator');
-    const methodDocs = DocGenerator.getMethodDocs('addEndpoint', classObj);
+    const methodDocs = DocGenerator.createMethodDocs('addEndpoint', classObj);
 
     expect(methodDocs?.hasOwnProperty('async')).toEqual(true);
     expect(methodDocs?.hasOwnProperty('comments')).toEqual(true);
