0542f40d6763c6e2521977541492f0fba6167ec9
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-12-18 21:50:25 +0100

    Fixed bugs in example converter

--- DocGenerator.ts
+++ DocGenerator.ts
@@ -662,3 +662,27 @@ export default new DocGenerator();
 
 const docs = new DocGenerator();
 docs.createAllDocsJson();
+
+// Convert examples to json
+const exampleDirectory = './src/examples/';
+const exampleJsonFile = './codeExamples.ts';
+
+export function convertExamplesToJson(dirname: string) {
+  fs.readdir(dirname, function (err, filenames) {
+    const data: { [x: string]: string } = {};
+
+    if (err) {
+      console.log(err);
+      return;
+    }
+    filenames.forEach(function (filename) {
+      const content = fs.readFileSync(dirname + filename, 'utf-8');
+      console.log(content);
+      const newFileName = filename.split('.')[0];
+      data[newFileName] = content;
+    });
+    fs.writeFileSync(exampleJsonFile, `export default ${JSON.stringify(data)};`);
+  });
+}
+
+convertExamplesToJson(exampleDirectory);
