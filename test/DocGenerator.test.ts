c070d259f1dee4640b52feb15608d791f582bfda
Rishabh Malik <rishabhmalik759@hotmail.com>
2022-12-19 07:12:23 +0100

    Updated exports and tests

--- test/DocGenerator.test.ts
+++ test/DocGenerator.test.ts
@@ -1,10 +1,9 @@
 import DocGenerator from '../DocGenerator';
 describe('Generate Docs class tests', () => {
   jest.setTimeout(50000);
-  it.only('Check if documentation object exist', async () => {
-    const classObj = DocGenerator.getClass('QorusDataProvider');
-    console.log(JSON.stringify(DocGenerator.getMethod('setPath', classObj)));
-    // expect(DocGenerator.createMethodDocs('getAllEndpoints', classObj)).not.toEqual(undefined);
+  it('Check if documentation object exist', async () => {
+    const classObj = DocGenerator.getClass('QorusAuthenticator');
+    expect(DocGenerator.createMethodDocs('addEndpoint', classObj)).not.toEqual(undefined);
   });
   it('Generate docs for Classes', async () => {
     const classDocs = DocGenerator.createClassDocs('QorusOptions');
